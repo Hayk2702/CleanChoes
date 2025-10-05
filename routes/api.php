597b29a57c63353a08ed23d6a65043e8cecc44da
Hayk Kocharyan <hayk.qocharyan.99@mail.ru>
2025-10-05 16:41:54 +0400

    commit

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix' => '/'], function () { //, 'middleware' => 'blockcors'
+Route::group(['prefix' => '/', 'middleware' => 'blockcors'], function () {
 
     Route::group(['middleware' => ['loggable']], function () {
 

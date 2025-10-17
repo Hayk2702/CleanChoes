17ad08b4e78c481452aeafdccb0d36b7213483da
Haykaz Yesayan <yesayanhaykaz@gmail.com>
2025-10-17 22:57:38 +0400

    commit

---
+++ storage/framework/views/653b777e7c1bfd0c0c7dfc90d9d3ac899d9dc2e2.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+    <main-component
+            :locale="'<?php echo e(app()->getLocale()); ?>'"
+            :authUser="<?php echo e(Auth::user()); ?>"
+    />
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/CleanChoes/resources/views/home.blade.php ENDPATH**/ ?>

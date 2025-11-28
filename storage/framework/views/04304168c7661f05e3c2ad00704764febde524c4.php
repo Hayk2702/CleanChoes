636085761191c324c5ac6dafc4ceddb069a1ec8b
hayk2702 <hayk.qocharyan.99@mail.ru>
2025-11-28 21:31:35 +0300

    server

---
+++ storage/framework/views/04304168c7661f05e3c2ad00704764febde524c4.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+    <main-component
+            :locale="'<?php echo e(app()->getLocale()); ?>'"
+            :authUser="<?php echo e(Auth::user()); ?>"
+    />
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/u3311901/data/www/admin.incleanshoes.ru/CleanChoes/resources/views/home.blade.php ENDPATH**/ ?>
